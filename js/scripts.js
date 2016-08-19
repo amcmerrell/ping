cc0a50ea9eb4173a18b9ecae9ec4f535f7e961bd
Andrew Merrell <andrew.m.merrell@gmail.com>
2016-08-19 10:16:17 -0700

    refactor replaceThree and replaceFive functions into single function that handles replacements for 3, 5, and 15.

--- js/scripts.js
+++ js/scripts.js
@@ -16,20 +16,35 @@ function countUp(maxValue) {
   return numberArray
 }
 
-function replaceThree(numberArray) {
-  for (i = 0; i < numberArray.length; i++) {
-    if (numberArray[i] % 3 === 0) {
-      numberArray.splice(i, 1, "ping");
+function replaceNumbers(numberArray) {
+  var replacedNums = [15, 5, 3];
+  var replacements = ["ping-pong", "pong", "ping"];
+
+  for (j = 0; j < replacedNums.length; j++) {
+    for (i = 0; i < numberArray.length; i++) {
+      if (numberArray[i] % replacedNums[j] === 0) {
+        numberArray.splice(i, 1, replacements[j]);
+      }
     }
   }
   return numberArray;
 }
 
-function replaceFive(numberArray) {
-  for (i = 0; i < numberArray.length; i++) {
-    if (numberArray[i] % 5 === 0) {
-      numberArray.splice(i, 1, "pong");
-    }
-  }
-  return numberArray;
-}
+// These functions have been replaced by refactored version
+// function replaceThree(numberArray) {
+//   for (i = 0; i < numberArray.length; i++) {
+//     if (numberArray[i] % 3 === 0) {
+//       numberArray.splice(i, 1, "ping");
+//     }
+//   }
+//   return numberArray;
+// }
+//
+// function replaceFive(numberArray) {
+//   for (i = 0; i < numberArray.length; i++) {
+//     if (numberArray[i] % 5 === 0) {
+//       numberArray.splice(i, 1, "pong");
+//     }
+//   }
+//   return numberArray;
+// }
